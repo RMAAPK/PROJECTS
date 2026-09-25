ce6efbc07c803c78e7993250d1d766f8b5e6212b
thealidev <thealidevmail@gmail.com>
2026-09-26 01:33:30 +0500

    fix: force add missing i18n and locales files

---
+++ Website/src/i18n.ts
@@ -0,0 +1,22 @@
+﻿import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import ko from './locales/ko.json';
+import ur from './locales/ur.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      ko: { translation: ko },
+      ur: { translation: ur }
+    },
+    lng: 'en', // default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+export default i18n;
